2ad84e093a3601d7525685bf4c5d72b136f36cf5
zilboe <1149845493@qq.com>
2024-07-24 21:55:32 +0800

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
