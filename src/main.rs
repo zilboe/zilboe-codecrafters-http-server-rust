8157a6c4f1296aa4ffc3f7b87a14da0593543ce9
zilboe <1149845493@qq.com>
2024-07-24 22:05:39 +0800

    200 res

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+
+fn handle_connect(mut stream: TcpStream) {
+    let mut read_buff: [u8; 1024] = [0; 1024];
+    let read_res = stream.read(&mut read_buff);
+    match read_res {
+        Ok(_) => {
+
+        }
+        Err(_) => {
+            eprintln!("recv from connect error");
+        }
+    }
+    let response = String::from("HTTP/1.1 200 OK\r\n\r\n");
+    let send_res = stream.write_all(response.as_bytes());
+    match send_res {
+        Ok(_) => {
+
+        }
+        Err(_) => {
+            eprintln!("response error");
+        }
+    };
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +36,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connect(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
