cc8e24370f7d8d848dbade463fa0ac357e796527
zilboe <1149845493@qq.com>
2024-07-30 21:52:32 +0800

    Gzip compression

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 // Uncomment this block to pass the first stage
 use std::{env, fs::{self, File}, io::{Read, Write}, net::{TcpListener, TcpStream}, path, thread};
-use flate2::write::{self, GzEncoder};
+use flate2::write::GzEncoder;
 use flate2::Compression;
-use nom::AsChar;
-fn stream_send(mut stream: TcpStream, buff: String) {
-    match stream.write_all(buff.as_bytes()) {
+fn stream_send(mut stream: TcpStream, send_buff: Vec<u8>) {
+    match stream.write_all(&send_buff) {
         Ok(_) => {
 
         }
@@ -29,7 +28,7 @@ fn handle_connect(mut stream: TcpStream) {
         }
     }
     let recv_split: Vec<&str> = recv_string.split("\r\n").collect();
-    let mut write_buff = String::new();
+    let mut write_buff: Vec<u8> = Vec::new();
     
     for recv_split_one in &recv_split {
         if recv_split_one.starts_with("Accept-Encoding:") {
@@ -44,45 +43,43 @@ fn handle_connect(mut stream: TcpStream) {
         }
     };
     if recv_split[0].starts_with("GET /index.html") {
-        write_buff.push_str("HTTP/1.1 200 OK\r\n\r\n");
+        write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
     } else if recv_split[0].starts_with("GET /echo/"){
-
-        write_buff.push_str("HTTP/1.1 200 OK\r\n");
-        write_buff.push_str("Content-Type: text/plain\r\n");
+        write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n".as_bytes());
+        write_buff.extend_from_slice("Content-Type: text/plain\r\n".as_bytes());
         if is_gzip {
-            write_buff.push_str("Content-Encoding: gzip\r\n");
+            write_buff.extend_from_slice("Content-Encoding: gzip\r\n".as_bytes());
         }
 
         let echo_head_len = "GET /echo/".len();
         let echo_head_str = &recv_split[0][echo_head_len..];
         let echo_str_split_space: Vec<&str> = echo_head_str.split(' ').collect();
         let echo_str_len = echo_str_split_space[0].len();
-        
-        
 
         if is_gzip {
-            let mut e = GzEncoder::new(Vec::new(), Compression::default());
-            e.write_all(echo_str_split_space[0].as_bytes()).expect("Turn Buff Gzip U8 Error");
+            let mut e = GzEncoder::new(vec![], Compression::default());
+            e.write_all(&echo_str_split_space[0].as_bytes()).expect("Turn Buff Gzip U8 Error");
             let write_encoder: Vec<u8> = e.finish().expect("gzip Error");
+            // println!("{:?}", write_encoder);
             let write_echo_str = format!("Content-Length: {}\r\n\r\n",write_encoder.len());
-            write_buff.push_str(&write_echo_str);
-            for i in write_encoder {
-                write_buff.push(i.as_char());
-            }
+            write_buff.extend_from_slice(write_echo_str.as_bytes());
+            write_buff.extend_from_slice(&write_encoder);
+            
         } else {
             let write_echo_str = format!("Content-Length: {}\r\n\r\n",echo_str_len);
-            write_buff.push_str(&write_echo_str);
-            write_buff.push_str(&echo_str_split_space[0]);
+            write_buff.extend_from_slice(write_echo_str.as_bytes());
+            write_buff.extend_from_slice(&echo_str_split_space[0].as_bytes());
         }
         
     } else if recv_split[0].starts_with("GET /user-agent"){
         for user_agent_split in recv_split {
             if user_agent_split.starts_with("User-Agent: ") {
-                write_buff.push_str("HTTP/1.1 200 OK\r\n");
-                write_buff.push_str("Content-Type: text/plain\r\n");
+                write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n".as_bytes());
+
+                write_buff.extend_from_slice("Content-Type: text/plain\r\n".as_bytes());
 
                 if is_gzip {
-                    write_buff.push_str("Content-Encoding: gzip\r\n");
+                    write_buff.extend_from_slice("Content-Encoding: gzip\r\n".as_bytes());
                 }
 
                 let user_agent_head_len: usize = "User-Agent: ".len();
@@ -91,20 +88,16 @@ fn handle_connect(mut stream: TcpStream) {
 
                 if is_gzip {
                     let mut e = GzEncoder::new(Vec::new(), Compression::default());
-                    e.write_all(user_agent_head_split[0].as_bytes()).expect("Turn Buff Gzip U8 Error");
+                    e.write_all(&user_agent_head_split[0].as_bytes()).expect("Turn Buff Gzip U8 Error");
                     let write_encoder: Vec<u8> = e.finish().expect("gzip Error");
                     let write_user_agent_str = format!("Content-Length: {}\r\n\r\n", write_encoder.len());
-                    write_buff.push_str(&write_user_agent_str);
-                    for i in write_encoder {
-                        write_buff.push(i.as_char());
-                    }
+                    write_buff.extend_from_slice(write_user_agent_str.as_bytes());
+                    write_buff.extend_from_slice(&write_encoder);
                 } else {
                     let write_user_agent_str = format!("Content-Length: {}\r\n\r\n", user_agent_head_len);
-                    write_buff.push_str(&write_user_agent_str);
-                    write_buff.push_str(&user_agent_head_split[0]);
+                    write_buff.extend_from_slice(write_user_agent_str.as_bytes());
+                    write_buff.extend_from_slice(user_agent_head_split[0].as_bytes());
                 }
-
-                
                 break;
             }
         };
@@ -118,11 +111,11 @@ fn handle_connect(mut stream: TcpStream) {
         file_name_head.push_str(file_head_split[0]);
         let path_file = path::Path::new(&file_name_head);
         if path_file.exists() {
-            write_buff.push_str("HTTP/1.1 200 OK\r\n");
-            write_buff.push_str("Content-Type: application/octet-stream\r\n");
+            write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n".as_bytes());
+            write_buff.extend_from_slice("Content-Type: application/octet-stream\r\n".as_bytes());
 
             if is_gzip {
-                write_buff.push_str("Content-Encoding: gzip\r\n");
+                write_buff.extend_from_slice("Content-Encoding: gzip\r\n".as_bytes());
             }
 
             let path_file_open = fs::OpenOptions::new().read(true).open(file_name_head);
@@ -142,16 +135,15 @@ fn handle_connect(mut stream: TcpStream) {
                     if is_gzip {
                         let mut e = GzEncoder::new(Vec::new(), Compression::default());
                         e.write_all(&file_open_buff).expect("Turn Buff Gzip U8 Error");
-                        let write_encoder: Vec<u8> = e.finish().expect("gzip Error");
+                        let write_encoder = e.finish().expect("gzip Error");
+                        // println!("{:?}", write_encoder);
                         let write_file_buff = format!("Content-Length: {}\r\n\r\n", write_encoder.len());
-                        write_buff.push_str(&write_file_buff);
-                        for i in write_encoder {
-                            write_buff.push(i.as_char());
-                        }
+                        write_buff.extend_from_slice(write_file_buff.as_bytes());
+                        write_buff.extend_from_slice(&write_encoder);
                     } else {
                         let write_file_buff = format!("Content-Length: {}\r\n\r\n", file_buffer_len);
-                        write_buff.push_str(&write_file_buff);
-                        write_buff.push_str(&file_open_buff_to_string);
+                        write_buff.extend_from_slice(write_file_buff.as_bytes());
+                        write_buff.extend_from_slice(file_open_buff_to_string.as_bytes());
                     }
 
                 }
@@ -162,7 +154,7 @@ fn handle_connect(mut stream: TcpStream) {
             }
 
         } else {
-            write_buff.push_str("HTTP/1.1 404 Not Found\r\n\r\n");
+            write_buff.extend_from_slice("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
         }
     } else if recv_split[0].starts_with("POST /files/") {
         let arg: Vec<String> = env::args().collect();   //env arg
@@ -187,14 +179,14 @@ fn handle_connect(mut stream: TcpStream) {
                 let request_post_content = &request_post_content[..get_post_content_size];
                 create_file.write(request_post_content.as_bytes()).expect("POST: write data to file error");
                 
-                write_buff.push_str("HTTP/1.1 201 Created\r\n\r\n");
+                write_buff.extend_from_slice("HTTP/1.1 201 Created\r\n\r\n".as_bytes());
             }
         }
     } else {
         if recv_split[0].starts_with("GET / ") {
-            write_buff.push_str("HTTP/1.1 200 OK\r\n\r\n");
+            write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
         } else {
-            write_buff.push_str("HTTP/1.1 404 Not Found\r\n\r\n");
+            write_buff.extend_from_slice("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
         }
     }
     
