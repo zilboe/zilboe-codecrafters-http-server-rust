7ee4632db5ee20cfc455ef6a933a909b2d70a71b
zilboe <1149845493@qq.com>
2024-08-06 22:16:52 +0800

    use tokio network,but not success

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,30 @@
 // Uncomment this block to pass the first stage
-use std::{env, fs::{self, File}, io::{Read, Write}, net::{TcpListener, TcpStream}, path, thread};
 use flate2::write::GzEncoder;
 use flate2::Compression;
-fn stream_send(mut stream: TcpStream, send_buff: Vec<u8>) {
-    match stream.write_all(&send_buff) {
-        Ok(_) => {
+use std::{
+    env,
+    fs::{self, File},
+    io::{Read, Write},
+    path,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-        }
-        Err(_) => {
-            eprintln!("response error");
-        }
-    }
+fn stream_send(mut stream: TcpStream, send_buff: Vec<u8>) {
+    println!("{:?}", stream);
+    stream.write_all(&send_buff);
 }
 
-
-fn handle_connect(mut stream: TcpStream) {
-    let mut read_buff: [u8; 1024] = [0; 1024];
+fn handle_connect(stream: TcpStream, read_buff: Vec<u8>) {
     let mut is_gzip: bool = false;
-    let read_res = stream.read(&mut read_buff);
     let recv_string = String::from_utf8((&read_buff).to_vec()).expect("utf-8 to string error");
     //println!("{}", recv_string);
-    match read_res {
-        Ok(_) => {
-        }
-        Err(_) => {
-            eprintln!("recv from connect error");
-        }
-    }
+
     let recv_split: Vec<&str> = recv_string.split("\r\n").collect();
     let mut write_buff: Vec<u8> = Vec::new();
-    
+
     for recv_split_one in &recv_split {
         if recv_split_one.starts_with("Accept-Encoding:") {
             let recv_split_skip_encoding_head = "Accept-Encoding:".len();
@@ -41,10 +36,10 @@ fn handle_connect(mut stream: TcpStream) {
                 }
             }
         }
-    };
+    }
     if recv_split[0].starts_with("GET /index.html") {
         write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-    } else if recv_split[0].starts_with("GET /echo/"){
+    } else if recv_split[0].starts_with("GET /echo/") {
         write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n".as_bytes());
         write_buff.extend_from_slice("Content-Type: text/plain\r\n".as_bytes());
         if is_gzip {
@@ -58,20 +53,19 @@ fn handle_connect(mut stream: TcpStream) {
 
         if is_gzip {
             let mut e = GzEncoder::new(vec![], Compression::default());
-            e.write_all(&echo_str_split_space[0].as_bytes()).expect("Turn Buff Gzip U8 Error");
+            e.write_all(&echo_str_split_space[0].as_bytes())
+                .expect("Turn Buff Gzip U8 Error");
             let write_encoder: Vec<u8> = e.finish().expect("gzip Error");
             // println!("{:?}", write_encoder);
-            let write_echo_str = format!("Content-Length: {}\r\n\r\n",write_encoder.len());
+            let write_echo_str = format!("Content-Length: {}\r\n\r\n", write_encoder.len());
             write_buff.extend_from_slice(write_echo_str.as_bytes());
             write_buff.extend_from_slice(&write_encoder);
-            
         } else {
-            let write_echo_str = format!("Content-Length: {}\r\n\r\n",echo_str_len);
+            let write_echo_str = format!("Content-Length: {}\r\n\r\n", echo_str_len);
             write_buff.extend_from_slice(write_echo_str.as_bytes());
             write_buff.extend_from_slice(&echo_str_split_space[0].as_bytes());
         }
-        
-    } else if recv_split[0].starts_with("GET /user-agent"){
+    } else if recv_split[0].starts_with("GET /user-agent") {
         for user_agent_split in recv_split {
             if user_agent_split.starts_with("User-Agent: ") {
                 write_buff.extend_from_slice("HTTP/1.1 200 OK\r\n".as_bytes());
@@ -83,27 +77,30 @@ fn handle_connect(mut stream: TcpStream) {
                 }
 
                 let user_agent_head_len: usize = "User-Agent: ".len();
-                let user_agent_head_split: Vec<&str> = user_agent_split[user_agent_head_len..].split(' ').collect();
+                let user_agent_head_split: Vec<&str> =
+                    user_agent_split[user_agent_head_len..].split(' ').collect();
                 let user_agent_head_len = user_agent_head_split[0].len();
 
                 if is_gzip {
                     let mut e = GzEncoder::new(Vec::new(), Compression::default());
-                    e.write_all(&user_agent_head_split[0].as_bytes()).expect("Turn Buff Gzip U8 Error");
+                    e.write_all(&user_agent_head_split[0].as_bytes())
+                        .expect("Turn Buff Gzip U8 Error");
                     let write_encoder: Vec<u8> = e.finish().expect("gzip Error");
-                    let write_user_agent_str = format!("Content-Length: {}\r\n\r\n", write_encoder.len());
+                    let write_user_agent_str =
+                        format!("Content-Length: {}\r\n\r\n", write_encoder.len());
                     write_buff.extend_from_slice(write_user_agent_str.as_bytes());
                     write_buff.extend_from_slice(&write_encoder);
                 } else {
-                    let write_user_agent_str = format!("Content-Length: {}\r\n\r\n", user_agent_head_len);
+                    let write_user_agent_str =
+                        format!("Content-Length: {}\r\n\r\n", user_agent_head_len);
                     write_buff.extend_from_slice(write_user_agent_str.as_bytes());
                     write_buff.extend_from_slice(user_agent_head_split[0].as_bytes());
                 }
                 break;
             }
-        };
-
+        }
     } else if recv_split[0].starts_with("GET /files") {
-        let arg: Vec<String> = env::args().collect();   //env arg
+        let arg: Vec<String> = env::args().collect(); //env arg
         let mut file_name_head = String::from(&arg[2]);
         let file_head_len = "GET /files".len();
         let file_head_str = &recv_split[0][file_head_len..];
@@ -130,55 +127,60 @@ fn handle_connect(mut stream: TcpStream) {
             match path_file_open.read_to_end(&mut file_open_buff) {
                 Ok(_) => {
                     let file_buffer_len = file_open_buff.len();
-                    let file_open_buff_to_string =  String::from_utf8_lossy(&file_open_buff);
+                    let file_open_buff_to_string = String::from_utf8_lossy(&file_open_buff);
 
                     if is_gzip {
                         let mut e = GzEncoder::new(Vec::new(), Compression::default());
-                        e.write_all(&file_open_buff).expect("Turn Buff Gzip U8 Error");
+                        e.write_all(&file_open_buff)
+                            .expect("Turn Buff Gzip U8 Error");
                         let write_encoder = e.finish().expect("gzip Error");
                         // println!("{:?}", write_encoder);
-                        let write_file_buff = format!("Content-Length: {}\r\n\r\n", write_encoder.len());
+                        let write_file_buff =
+                            format!("Content-Length: {}\r\n\r\n", write_encoder.len());
                         write_buff.extend_from_slice(write_file_buff.as_bytes());
                         write_buff.extend_from_slice(&write_encoder);
                     } else {
-                        let write_file_buff = format!("Content-Length: {}\r\n\r\n", file_buffer_len);
+                        let write_file_buff =
+                            format!("Content-Length: {}\r\n\r\n", file_buffer_len);
                         write_buff.extend_from_slice(write_file_buff.as_bytes());
                         write_buff.extend_from_slice(file_open_buff_to_string.as_bytes());
                     }
-
                 }
                 Err(_) => {
                     println!("read file to buffer error");
                     return;
                 }
             }
-
         } else {
             write_buff.extend_from_slice("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
         }
     } else if recv_split[0].starts_with("POST /files/") {
-        let arg: Vec<String> = env::args().collect();   //env arg
-        let file_name_head = String::from(&arg[2]);     //Get ENV Path
-        let post_head_len = "POST /files/".len();           
-        let post_head_str_split = &recv_split[0][post_head_len..];      //Get POST request index
-        let post_head_str: Vec<&str> = post_head_str_split.split(' ').collect();    //Get POST request filename
+        let arg: Vec<String> = env::args().collect(); //env arg
+        let file_name_head = String::from(&arg[2]); //Get ENV Path
+        let post_head_len = "POST /files/".len();
+        let post_head_str_split = &recv_split[0][post_head_len..]; //Get POST request index
+        let post_head_str: Vec<&str> = post_head_str_split.split(' ').collect(); //Get POST request filename
 
-        let post_head_filename = file_name_head + &post_head_str[0];    //Get The Whole Filename including path
-        
-        
-        let request_post_content = recv_split.len();    //Get POST request Content
-        let request_post_content = recv_split[request_post_content-1];  //Get POST content
-        //println!("{}", post_head_filename);
+        let post_head_filename = file_name_head + &post_head_str[0]; //Get The Whole Filename including path
+
+        let request_post_content = recv_split.len(); //Get POST request Content
+        let request_post_content = recv_split[request_post_content - 1]; //Get POST content
+                                                                         //println!("{}", post_head_filename);
         for get_post_recv_head in recv_split {
             if get_post_recv_head.starts_with("Content-Length:") {
                 let skip_post_content_len = "Content-Length: ".len();
                 let get_post_content_size = &get_post_recv_head[skip_post_content_len..];
-                let get_post_content_size: usize = get_post_content_size.parse().expect("POST: Get content-len Error");
+                let get_post_content_size: usize = get_post_content_size
+                    .parse()
+                    .expect("POST: Get content-len Error");
 
-                let mut create_file: File = fs::File::create(post_head_filename.clone()).expect("create file error");
+                let mut create_file: File =
+                    fs::File::create(post_head_filename.clone()).expect("create file error");
                 let request_post_content = &request_post_content[..get_post_content_size];
-                create_file.write(request_post_content.as_bytes()).expect("POST: write data to file error");
-                
+                create_file
+                    .write(request_post_content.as_bytes())
+                    .expect("POST: write data to file error");
+
                 write_buff.extend_from_slice("HTTP/1.1 201 Created\r\n\r\n".as_bytes());
             }
         }
@@ -189,29 +191,32 @@ fn handle_connect(mut stream: TcpStream) {
             write_buff.extend_from_slice("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
         }
     }
-    
+
     stream_send(stream, write_buff);
-    
-    
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    
+
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:4221").await?;
     //
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                thread::spawn(|| {handle_connect(_stream)});
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
-    };
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut byte_r = [0; 1024];
+            let n = match socket.read(&mut byte_r).await {
+                Ok(n) if n == 0 => return,
+                Ok(n) => n,
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    return;
+                }
+            };
+            handle_connect(socket, (&byte_r[0..n]).to_vec());
+        });
+    }
 }
