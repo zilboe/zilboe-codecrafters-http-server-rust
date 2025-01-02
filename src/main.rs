0bfd6334e73f16edd6b06b8f7fcb4bb4f526a018
zilboe <1149845493@qq.com>
2025-01-02 23:29:35 +0800

    Some Error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // Uncomment this block to pass the first stage
 use flate2::write::{self, GzEncoder};
 use flate2::Compression;
-use nom::{FindSubstring};
-use std::{path::Path, ptr::eq, env, io};
 use httparse::Request;
+use nom::FindSubstring;
+use std::io::Read;
+use std::path::Path;
+use std::{env, fs::File, io, ptr::eq};
+use std::{fs, vec};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
@@ -17,20 +20,21 @@ struct MyError {
     err_data: Vec<u8>,
 }
 impl From<io::Error> for MyError {
-    fn from(error: io::Error) -> Self{
-        MyError { 
-            message: error.to_string(), 
+    fn from(error: io::Error) -> Self {
+        MyError {
+            message: error.to_string(),
             err_data: vec![],
         }
     }
 }
 
 /// HTTP Config, it doesn't require much configuration
-struct WebRequest{
+struct WebRequest {
     http_stream: TcpStream,
     keep_alive: bool,
     is_gzip: bool,
     uri_path: Option<String>,
+    file_exist: bool,
 }
 
 impl WebRequest {
@@ -40,19 +44,21 @@ impl WebRequest {
             keep_alive: false,
             is_gzip: false,
             uri_path: None,
+            file_exist: false,
         }
     }
 
-    // The entire access path and access parameter 
-    // processing need to determine the local file 
+    // The entire access path and access parameter
+    // processing need to determine the local file
     // and needs to fill in the content-type tag
     fn get_path(&mut self, uri_path: &str) -> Result<Vec<u8>, MyError> {
         let mut uri_path_name = String::from(uri_path);
 
-        if uri_path.ends_with('/') {    //??? i think this need considering the Multiple '/'
+        if uri_path.ends_with('/') {
+            //??? i think this need considering the Multiple '/'
             uri_path_name += "index.html";
         }
-        
+
         match self.set_file_path_isexist(&uri_path_name) {
             Ok(mut write_buff) => {
                 //then i need to analy the request file type
@@ -75,30 +81,29 @@ impl WebRequest {
                             _ => "application/octet-stream",
                         };
                         write_buff.extend_from_slice(file_type.as_bytes());
-                    },
+                    }
                     None => {
                         write_buff.extend_from_slice(b"application/octet-stream");
                     }
                 };
                 write_buff.extend_from_slice(b"\r\n");
                 Ok(write_buff)
-            },
-
-            Err(e) => {
-                Err(e)
             }
-        }//404 NO FOUND
+
+            Err(e) => Err(e),
+        } //404 NO FOUND
     }
 
-    // need to check that the file exists and pass 
+    // need to check that the file exists and pass
     // the full file name to the uri_path parameter
-    // and pass the response code 200 and 404 
+    // and pass the response code 200 and 404
     fn set_file_path_isexist(&mut self, file_name: &str) -> Result<Vec<u8>, MyError> {
         let env_arg: Vec<String> = env::args().collect();
         let mut response_code: Vec<u8> = Vec::new();
         if env_arg.len() < 2 {
+            self.file_exist = false;
             response_code.extend_from_slice(b"HTTP/1.1 404 OK\r\n\r\n");
-            Err(MyError{
+            Err(MyError {
                 message: "404 NO FOUND".to_string(),
                 err_data: response_code,
             })
@@ -107,16 +112,73 @@ impl WebRequest {
             let env_file_path = env_arg[1].as_str().to_owned() + take_uri_path;
             let path_file: &Path = Path::new(&env_file_path);
             if path_file.exists() {
+                println!("{:?}", self.uri_path);
                 self.uri_path = self.uri_path.replace(env_file_path);
+                println!("{:?}", self.uri_path);
+                self.file_exist = true;
                 response_code.extend_from_slice(b"HTTP/1.1 200 OK\r\n");
                 Ok(response_code)
             } else {
                 response_code.extend_from_slice(b"HTTP/1.1 404 OK\r\n\r\n");
-                Err(MyError{
+                self.file_exist = false;
+                Err(MyError {
                     message: "404 FILE NO FOUND".to_string(),
                     err_data: response_code,
                 })
             }
+        }
+    }
+
+    fn fill_file_buffer(&mut self) -> Result<Vec<u8>, MyError> {
+        let mut file_buffer: Vec<u8> = Vec::new();
+        println!("{:?}", self.uri_path);
+        if self.file_exist {
+            let path = match &self.uri_path {
+                Some(path) => path,
+                None => {
+                    return Err(MyError {
+                        message: "Don't Open The File".to_string(),
+                        err_data: file_buffer,
+                    })
+                }
+            };
+            match fs::OpenOptions::new().read(true).open(path) {
+                Ok(mut open_file) => match open_file.read_to_end(&mut file_buffer) {
+                    Ok(_) => {
+                        if self.is_gzip {
+                            Ok(file_buffer)
+                        } else {
+                            let write_file_size: Vec<u8> =
+                                format!("Content-Length: {}\r\n\r\n", file_buffer.len())
+                                    .as_bytes()
+                                    .to_vec();
+                            let write_buffer = vec![write_file_size, file_buffer].concat();
+                            println!("{:?}", write_buffer);
+                            Ok(write_buffer)
+                        }
+                    }
+                    Err(e) => {
+                        file_buffer.extend_from_slice(b"Content-Length: 0\r\n\r\n");
+                        Err(MyError {
+                            message: e.to_string(),
+                            err_data: file_buffer,
+                        })
+                    }
+                },
+                Err(e) => {
+                    file_buffer.extend_from_slice(b"Content-Length: 0\r\n\r\n");
+                    Err(MyError {
+                        message: e.to_string(),
+                        err_data: file_buffer,
+                    })
+                }
+            }
+        } else {
+            file_buffer.extend_from_slice(b"Content-Length: 0\r\n\r\n");
+            Err(MyError {
+                message: "Don't Open The File".to_string(),
+                err_data: file_buffer,
+            })
         }
     }
 
@@ -132,12 +194,12 @@ impl WebRequest {
         Ok(())
     }
 
-    // The entire response header portion is processed from the 
-    // time the recv_buffer is received until the data is finally processed 
+    // The entire response header portion is processed from the
+    // time the recv_buffer is received until the data is finally processed
     // and the data to be sent is returned.
     // Use the parsing method of httparse library, it is zero copy parsing,
-    // so the speed will be relatively faster, 
-    // at present is about the use of each request processing if, 
+    // so the speed will be relatively faster,
+    // at present is about the use of each request processing if,
     // then consider modifying to a more standard processing
     fn fill_response(&mut self, recv_buff: &[u8]) -> Vec<u8> {
         // we need parse the header, so i alloc httparse named header
@@ -160,19 +222,25 @@ impl WebRequest {
 
         for header_item in header {
             if header_item.name.eq_ignore_ascii_case("Accept-Encoding") {
-                    let _ = self.set_gzip_config(std::str::from_utf8(header_item.value).unwrap());
+                let _ = self.set_gzip_config(std::str::from_utf8(header_item.value).unwrap());
             }
             if header_item.name.eq_ignore_ascii_case("keep-alive") {
-                    let _ = self.set_keepalive_config(std::str::from_utf8(header_item.value).unwrap());
+                let _ = self.set_keepalive_config(std::str::from_utf8(header_item.value).unwrap());
             }
         }
-        
+
         // Whether to select file compression
         if self.is_gzip {
             write_buff.extend_from_slice(b"Content-Encoding: gzip\r\n");
         }
-        
-        write_buff
+        let file_buff = match self.fill_file_buffer() {
+            Ok(file_buff) => file_buff,
+            Err(e) => {
+                println!("{}", e.message);
+                e.err_data
+            }
+        };
+        vec![write_buff, file_buff].concat()
     }
 
     async fn send(&mut self, write_buff: &[u8]) -> io::Result<()> {
@@ -182,15 +250,14 @@ impl WebRequest {
 
     async fn close(&mut self) {
         // if !self.keep_alive {
-            let _ = self.http_stream.shutdown().await;
+        let _ = self.http_stream.shutdown().await;
         // }
     }
-
 }
 
-async fn process_content(stream: TcpStream, recv_buff: &[u8]){
+async fn process_content(stream: TcpStream, recv_buff: &[u8]) {
     let mut one_request = WebRequest::new(stream);
-    let send_buffer= one_request.fill_response(recv_buff);
+    let send_buffer = one_request.fill_response(recv_buff);
     let _ = one_request.send(&send_buffer).await;
     one_request.close().await;
 }
@@ -199,7 +266,7 @@ async fn handle_connect(mut stream: TcpStream) {
     let mut read_buff: [u8; 512] = [0; 512];
     let _ = stream.read(&mut read_buff).await;
     process_content(stream, &read_buff).await;
-    
+
     // let _ = stream.shutdown().await;
 }
 
